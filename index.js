b6bf9b3809c4ebe5f92ef7ee6921162e91a8d75d
zahraaAed <zahraaalaaeddine94@gmail.com>
2024-12-21 16:11:47 +0200

    step4

--- index.js
+++ index.js
@@ -22,6 +22,26 @@ app.get('/time', (req,res)=>{
 
 
 });
+
+app.get('/hello/:id?', (req, res) => {
+    const { id } = req.params;
+    if (id) {
+      res.json({ status: 200, message: `Hello, ${id}` });
+    } else {
+      res.json({ status: 200, message: 'Hello!' });
+    }
+  });
+
+  app.get('/search', (req, res) => {
+    const { s } = req.query;
+    if (s) {
+      res.status(200).json({ status: 200, message: 'ok', data: s });
+    } else {
+      res.status(500).json({ status: 500, error: true, message: 'you have to provide a search' });
+    }
+  });
+
+
 // Start the server and listen on the defined port
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
