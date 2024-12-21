5c1962015750899cb33688a8b04c797a59ef4565
zahraaAed <zahraaalaaeddine94@gmail.com>
2024-12-21 15:04:22 +0200

    adding point2 to step 2

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express=require("express");
 const app=express();
 const PORT=3000;
 
+app.get('/', (req, res) => {
+    res.send('ok')
+  })
+
+
+
+
+
+
+
 // Start the server and listen on the defined port
 app.listen(PORT, () => {
     console.log(`Server is running on PORT:${PORT}`);
