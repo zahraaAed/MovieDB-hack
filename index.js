8d9699cdefc03d6d1099eca6e68d6a7972a055d8
zahraaAed <zahraaalaaeddine94@gmail.com>
2024-12-21 15:44:29 +0200

    step3

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ const PORT=3000;
 
 app.get('/', (req, res) => {
     res.send('ok')
-  })
+  });
 
 
+app.get('/test', (req,res)=>{
+    res.json({ status: 200, message: "ok" });
+});
+
+app.get('/time', (req,res)=>{
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    
+    const currentTime = `${hours}:${minutes}`;
+    
+    res.json({ status: 200, message: currentTime });
 
 
-
-
-
+});
 // Start the server and listen on the defined port
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
